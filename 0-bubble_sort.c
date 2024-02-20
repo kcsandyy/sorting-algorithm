6f5cafb919b8227e6ca642173df4708b482ef539
kcsandyy <kemboisandra20@gmail.com>
2024-02-20 04:34:53 -0800

    Insertion sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,30 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort array lements from min to max value
+ * bubble_sort - sort array from min to max value
  * @array: array
  * @size: array size
  */
 
-void bubble_sort(int *array, size_t size)
+Void bubble_sort(int *array, size_t size)
 {
-	size_t i, index, tmp = 0;
+	size_t i = 0;
+	size_t index = 0;
+	size_t tmp = 0;
 
 	if (size < 2)
 		return;
 	for (i = 0; i < size; i++)
+	{
 		for (index = 0; index < size; index++)
 		{
 			if (array[index] > array[index + 1] && array[index + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				tmp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = tmp;
+				print_array(array, size);
 			}
 		}
+	}
 }
